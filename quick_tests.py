81f0696a0f2b65a7a8b087b6678f9903623a018d
Alaa-Arees-PHOENIX <alaa15arees@gmail.com>
2026-06-09 13:00:58 +0300

    Added more sample users for quick testing

--- quick_tests.py
+++ quick_tests.py
@@ -1,5 +1,5 @@
 quickTests = [
-        {
+        { #1
             'budget':'medium',
             'preferred_music_style':'classic',
             'like_wind_instruments':True,
@@ -10,5 +10,57 @@ quickTests = [
             'like_to_play_with_group':True,
             'expected_time_of_training':'long',
             'enjoy_creating_harmonies':True
-        }
+        },
+
+        { #2
+            'budget':'low',
+            'preferred_music_style':'oriental',
+            'like_wind_instruments':True,
+            'like_percussion_instruments':True,
+            'travels_alot':False,
+            'complaining_neighbors':False,
+            'lungs_capacity':'high',
+            'like_to_play_with_group':True,
+            'expected_time_of_training':'medium',
+            'enjoy_creating_harmonies':False
+        },
+
+        { #3
+            'budget':'high',
+            'preferred_music_style':'jazz',
+            'like_wind_instruments':True,
+            'like_percussion_instruments':False,
+            'travels_alot':True,
+            'complaining_neighbors':False,
+            'lungs_capacity':'low',
+            'like_to_play_with_group':False,
+            'expected_time_of_training':'medium',
+            'enjoy_creating_harmonies':True
+        },
+
+        { #4
+            'budget':'medium',
+            'preferred_music_style':'rock',
+            'like_wind_instruments':False,
+            'like_percussion_instruments':True,
+            'travels_alot':True,
+            'complaining_neighbors':False,
+            'lungs_capacity':'high',
+            'like_to_play_with_group':True,
+            'expected_time_of_training':'short',
+            'enjoy_creating_harmonies':False
+        },
+
+        { #5
+            'budget':'low',
+            'preferred_music_style':'folk',
+            'like_wind_instruments':True,
+            'like_percussion_instruments':True,
+            'travels_alot':False,
+            'complaining_neighbors':True,
+            'lungs_capacity':'high',
+            'like_to_play_with_group':True,
+            'expected_time_of_training':'short',
+            'enjoy_creating_harmonies':True
+        },
 ]
